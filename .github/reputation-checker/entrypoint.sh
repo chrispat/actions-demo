6e656c19e5c0c52a854d301cfaf72fdfb5732fe9
Chris Patterson <chrispat@microsoft.com>
2018-11-06 23:05:52 -0500

    Inital action implementation

---
+++ .github/reputation-checker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+$user_url=$(cat $GITHUB_EVENT_PATH | jq -r .comment.user.url)
+
+$followers=$(curl -s $user_url | jq -r .followers)
+
+labels_url=$(cat $GITHUB_EVENT_PATH | jq -r .issue.url)/labels
+
+if [$followers -gt 10]
+then
+    echo "High reputation user"
+    curl -s -H "Authorization: token "$GITHUB_TOKEN \
+        -H "Accept: application/json" \
+        --request POST \
+        --data '{ "labels": ["high-reputation"]}' $labels_url
+else
+    echo "Low reputation user"
+fi
+
+
